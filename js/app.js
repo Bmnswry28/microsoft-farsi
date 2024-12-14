47bf8b603faf642a3f520a9fbea7c372ad829229
abbas <bmnswry27@gmail.com>
2024-12-14 03:51:46 +0330

    done

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function initMap() {
+    var mapOptions = {
+        center: { lat: , lng:  }, 
+        zoom: 14
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
